b90c584a00639bb10e1e77226b0943c70dca5d3a
Saurabh Kumar Singh <saurabh.singh@team.cliffex.com>
2022-07-25 18:32:41 +0530

    change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./component//config/firebase-config";
-import { BrowserRouter, Routes, Route, NavLink, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
